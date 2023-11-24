00a1f77e7f66c284b50a94cf109956ead1c25dbe
Kiranlehal <Kirandeepkaur.Lehal@triosstudent.com>
2023-11-24 09:00:59 -0500

    v1

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿// For more imformation see https://aka.ms/fsharp-console-apps
+type Cch = {
+    Name: string
+    Formerplayer: bool
+}
+
+type Sts = {
+    wins: int 
+    losses: int
+}
+
+type tm = {
+    Name: string
+    Cch: Cch
+    Sts: Sts
+}
