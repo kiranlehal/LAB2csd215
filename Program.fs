7d592b2280a25d5b99aed62aa392e0399ed1dd53
Kiranlehal <Kirandeepkaur.Lehal@triosstudent.com>
2023-11-24 09:52:07 -0500

    v1

--- Program.fs
+++ Program.fs
@@ -16,10 +16,19 @@ type tm = {
 }
 
 let tm = [
-    { Name = "Boston Celtics"; Cch = {Name = "Joe Mazzulla"; Formerplayer = false}; Sts = {wins = 3,570; losses = 2,462}}
-    { Name = "Charlotte Hornets"; Cch = {Name = "Steve Clifford"; Formerplayer = true}; Sts = {wins = 1,153; losses = 1,478}}
-    { Name = "Cleveland Cavaliers"; Cch = {Name = "J.B. Bickerstaff"; Formerplayer = true}; Sts = {wins = 1,984; losses = 2,287}}
-    { Name = "	Denver Nuggets"; Cch = {Name = "Michael Malone"; Formerplayer = true}; Sts = {wins = 1,897; losses = 1,890}}
-    { Name = "Golden State Warriors"; Cch = {Name = "Steve Kerr"; Formerplayer = true}; Sts = {wins = 2,923; losses = 3,098}}
+    { Name = "Boston Celtics"; Cch = {Name = "Joe Mazzulla"; Formerplayer = false}; Sts = {wins = 3570; losses = 2462}}
+    { Name = "Charlotte Hornets"; Cch = {Name = "Steve Clifford"; Formerplayer = true}; Sts = {wins = 1153; losses = 1478}}
+    { Name = "Cleveland Cavaliers"; Cch = {Name = "J.B. Bickerstaff"; Formerplayer = true}; Sts = {wins = 1984; losses = 2287}}
+    { Name = "Denver Nuggets"; Cch = {Name = "Michael Malone"; Formerplayer = true}; Sts = {wins = 1897; losses = 1890}}
+    { Name = "Golden State Warriors"; Cch = {Name = "Steve Kerr"; Formerplayer = true}; Sts = {wins = 2923; losses = 3098}}
 ]
+
+let goodtm = tm |> List.filter (fun tm -> tm.Sts.wins > tm.Sts.losses)
+
+//printfn "%A" goodtm
+
+goodtm |> List.iter (fun tm -> printfn "tm Name: %s" tm.Name)
+goodtm |> List.iter (fun tm -> printfn "tm wins: %d" tm.Sts.wins)
+
+
 
